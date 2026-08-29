aff91d9458cf1d84c3f632209b8c55b93d2658f6
Kazukii-afk <Kasuki9225@proton.me>
2026-08-29 14:25:43 +0200

    Add personal information and age update in main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+nom="Kazukii"
+age=20
+print(f"Je m'appelle {Kazukii} et j'ai {20} ans.")# Écrivez votre code ici !
+age = age + 10
+print(f"Je m'appelle {Kazukii} et j'ai {30} ans maintenant.")
