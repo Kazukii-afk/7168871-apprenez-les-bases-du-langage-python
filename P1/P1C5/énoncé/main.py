5baa302946642f04fadae640b893dd09cd10b81e
Kazukii-afk <Kasuki9225@proton.me>
2026-08-29 14:31:40 +0200

    Initialize user info and display details
    Added variables for name, age, height, and student status with print statements to display their values and types.

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom="Alex"# Écrivez votre code ici !
+age=22
+taille=1,75
+est_etudiant= True
+print(f"Nom : {nom}")
+print(f"Âge: {age}")
+print(f"Queue : {taille}")
+print(f"Est étudiant: {est_etudiant}")
+print(f"Type nom: {type(nom)}")
+print(f"Saisissez l'âge : {type(age)}")
+print(f"Type taille : {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
