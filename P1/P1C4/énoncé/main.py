1417116e86d6eda3caa35739ed10e25dc46f0718
Kazukii-afk <Kasuki9225@proton.me>
2026-08-30 14:28:32 +0200

    Add name and age print statements
    Fix syntax errors and update age calculation.

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+nom="Kazukii"
+age=20
+print(f"je m'apelle {nom} et j'ai {age} ans."
+     age = age+10)# Écrivez votre code ici !
+     print(f"Je m'apelle {nom} et j'ai {age} maintenant.)
+     
